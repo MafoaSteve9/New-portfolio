b444b7257bba17c7e3bd0862fb7fd797c384e3d8
Steve Mafoa <steve_mafoa@icloud.com>
2024-11-22 20:03:21 +0100

    - Ajout des media quéries pour les différents écrans✅ - Modifications du html pour la mise en place de nos composant✅ - Supression du title et du composant Theme ✅

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { ThemesComponent } from './components/themes/themes.component';
+import { ContactComponent } from "./components/contact/contact.component";
+import { AboutComponent } from "./components/about/about.component";
+import { ProjetComponent } from "./components/projet/projet.component";
+import { WorkComponent } from "./components/work/work.component";
+import { CertificatComponent } from "./components/certificat/certificat.component";
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ThemesComponent],
+  imports: [ContactComponent, AboutComponent, ProjetComponent, WorkComponent, CertificatComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
